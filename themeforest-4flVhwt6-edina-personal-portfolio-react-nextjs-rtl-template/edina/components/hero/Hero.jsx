e7ca907dd1dfadc6192fe3ce3238a106b45bf082
Brad Stricherz <Brad.Stricherz@gmail.com>
2024-09-19 17:03:02 -0500

    Updating Hero component

--- themeforest-4flVhwt6-edina-personal-portfolio-react-nextjs-rtl-template/edina/components/hero/Hero.jsx
+++ themeforest-4flVhwt6-edina-personal-portfolio-react-nextjs-rtl-template/edina/components/hero/Hero.jsx
@@ -6,85 +6,84 @@ import shapeImage from "../../public/img/hero/1.jpg";
 import heroImage from "../../public/img/hero/2.jpg";
 
 const heroContent = {
-  shapeImage: shapeImage,
-  heroImage: heroImage,
-  name: "Lauren Shores",
-  description: `I'm a Data Scientist based In USA, with over 16 years of
-  professional experience.`,
+    shapeImage: shapeImage,
+    heroImage: heroImage,
+    name: "Lauren Shores",
+    description: `Harnessing data to create understanding and provide solutions for over 8 years.`,
 };
 
 const Hero = () => {
-  return (
-    //    HERO
-    <div className="edina_tm_hero" id="home">
-      <div className="content">
-        <div className="img-shape" data-aos="fade-up" data-aos-duration="1200">
-          <Image src={heroContent.heroImage} alt="brand" />
-        </div>
-        <div className="extra">
-          <h5
-            data-aos="fade-up"
-            data-aos-duration="1200"
-            data-aos-delay="100"
-            className="hello"
-          >
-            Hello, I&apos;m {heroContent.name}
-          </h5>
-          <h1
-            className="name"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-            data-aos-delay="200"
-          >
+    return (
+        //    HERO
+        <div className="edina_tm_hero" id="home">
+            <div className="content">
+                <div className="img-shape" data-aos="fade-up" data-aos-duration="1200">
+                    <Image src={heroContent.heroImage} alt="brand"/>
+                </div>
+                <div className="extra">
+                    <h3
+                        data-aos="fade-up"
+                        data-aos-duration="1200"
+                        data-aos-delay="100"
+                        className="hello"
+                    >
+                        {heroContent.name}
+                    </h3>
+                    <h1
+                        className="name"
+                        data-aos="fade-up"
+                        data-aos-duration="1200"
+                        data-aos-delay="200"
+                    >
             <span className="typer-toper">
               <ReactTyped
-                loop
-                typeSpeed={150}
-                backSpeed={60}
-                strings={["Data Scientist", "Mega Babe"]}
-                smartBackspace
-                shuffle={false}
-                backDelay={1}
-                fadeOut={false}
-                fadeOutDelay={100}
-                loopCount={0}
-                showCursor
-                cursorChar="|"
+                  loop
+                  typeSpeed={150}
+                  backSpeed={60}
+                  strings={["Data Scientist", "Instructor", "Speaker"]}
+                  smartBackspace
+                  shuffle={false}
+                  backDelay={1}
+                  fadeOut={false}
+                  fadeOutDelay={100}
+                  loopCount={0}
+                  showCursor
+                  cursorChar="|"
               />
             </span>
-          </h1>
-          <p
-            className="text"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-            data-aos-delay="300"
-          >
-            {heroContent.description}
-          </p>
+                    </h1>
+                    <p
+                        className="text"
+                        data-aos="fade-up"
+                        data-aos-duration="1200"
+                        data-aos-delay="300"
+                    >
+                        {heroContent.description}
+                    </p>
 
-          <div
-            className="social"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-            data-aos-delay="400"
-          >
-            <Social />
-          </div>
-          <div
-            className="edina_tm_button"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-            data-aos-delay="500"
-          >
-            <a href="/img/sample.pdf" download className="color">
-              Download CV
-            </a>
-          </div>
+                    <div
+                        className="social"
+                        data-aos="fade-up"
+                        data-aos-duration="1200"
+                        data-aos-delay="400"
+                    >
+                        <Social/>
+                    </div>
+                    <div
+                        className="edina_tm_button"
+                        data-aos="fade-up"
+                        data-aos-duration="1200"
+                        data-aos-delay="500"
+                    >
+                        <a href="/img/sample.pdf" download className="color">
+                            Download CV
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    // /HERO
-  );
+        // /HERO
+    );
 };
 
 export default Hero;
