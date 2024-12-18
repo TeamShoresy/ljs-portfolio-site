3313d89cf6c855c4d113ea88f59550a73d80f0d0
Lauren Shores <ljshores@users.noreply.github.com>
2024-12-18 09:12:48 -0600

    Update index.jsx

--- components/pages/home-sidebar/index.jsx
+++ components/pages/home-sidebar/index.jsx
@@ -71,9 +71,7 @@ const EdinaHomeSidebar = () => {
                     <div className="edina_tm_title">
                         <h3>Portfolio</h3>
                         <p>
-                            Data scientist experienced in supply chain and real estate, leading impactful machine
-learning projects, mentoring Black professionals in data, and exploring insights on race,
-place, and food.
+                            Data science and analytics projects of interest.
                         </p>
                     </div>
                     {/* End edian_tm_title */}
